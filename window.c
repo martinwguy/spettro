b122e09ecb3bc7891ee18725d8b576aa3467e4cd
Martin Guy <martinwguy@gmail.com>
2017-06-09 16:33:50 +0200

    Reformat from sndfile-tools style to standard C layout.

--- window.c
+++ window.c
@@ -23,145 +23,123 @@
 #include "spettro.h"
 #include "window.h"
 
-#define ARRAY_LEN(x)		((int) (sizeof (x) / sizeof (x [0])))
+#define ARRAY_LEN(x)		((int) (sizeof(x) / sizeof(x[0])))
 
 
-static double besseli0 (double x) ;
-static double factorial (int k) ;
+static double besseli0(double x);
+static double factorial(int k);
 
 void
-calc_kaiser_window (double * data, int datalen, double beta)
+calc_kaiser_window(double *data, int datalen, double beta)
 {
-	/*
-	**          besseli0 (beta * sqrt (1- (2*x/N).^2))
-	** w (x) =  --------------------------------------,  -N/2 <= x <= N/2
-	**                 besseli0 (beta)
-	*/
+    /*
+     *         besseli0(beta * sqrt(1 - (2*x/N).^2))
+     * w(x) =  --------------------------------------,  -N/2 <= x <= N/2
+     *                 besseli0(beta)
+     */
 
-	double two_n_on_N, denom ;
-	int k ;
+    double two_n_on_N, denom;
+    int k;
 
-	denom = besseli0 (beta) ;
+    denom = besseli0(beta);
 
-	if (! isfinite (denom))
-	{	printf ("besseli0 (%f) : %f\nExiting\n", beta, denom) ;
-		exit (1) ;
-		} ;
+    if (!isfinite(denom)) {
+	printf("besseli0(%f) : %f\nExiting\n", beta, denom);
+	exit(1);
+    }
 
-	for (k = 0 ; k < datalen ; k++)
-	{	double n = k + 0.5 - 0.5 * datalen ;
-		two_n_on_N = (2.0 * n) / datalen ;
-		data [k] = besseli0 (beta * sqrt (1.0 - two_n_on_N * two_n_on_N)) / denom ;
-		} ;
+    for (k = 0; k < datalen ; k++) {
+	double n = k + 0.5 - 0.5 * datalen;
+	two_n_on_N = (2.0 * n) / datalen;
+	data[k] = besseli0(beta * sqrt(1.0 - two_n_on_N * two_n_on_N)) / denom;
+    }
 
-	return ;
+    return;
 } /* calc_kaiser_window */
 
 void
-calc_nuttall_window (double * data, int datalen)
+calc_nuttall_window(double *data, int datalen)
 {
-	const double a [4] = { 0.355768, 0.487396, 0.144232, 0.012604 } ;
-	int k ;
+    const double a[4] = { 0.355768, 0.487396, 0.144232, 0.012604 };
+    int k;
 
-	/*
-	**	Nuttall window function from :
-	**
-	**	http://en.wikipedia.org/wiki/Window_function
-	*/
+    /*
+     *	Nuttall window function from :
+     *
+     *	http://en.wikipedia.org/wiki/Window_function
+     */
 
-	for (k = 0 ; k < datalen ; k++)
-	{	double scale ;
+    for (k = 0; k < datalen ; k++) {
+    	double scale;
 
-		scale = M_PI * k / (datalen - 1) ;
+	scale = M_PI * k / (datalen - 1);
 
-		data [k] = a [0] - a [1] * cos (2.0 * scale) + a [2] * cos (4.0 * scale) - a [3] * cos (6.0 * scale) ;
-		} ;
+	data[k] = a[0]
+		- a[1] * cos(2.0 * scale)
+		+ a[2] * cos(4.0 * scale)
+		- a[3] * cos(6.0 * scale);
+    }
 
-	return ;
-} /* calc_nuttall_window */
+    return;
+}
 
 void
-calc_hann_window (double * data, int datalen)
+calc_hann_window(double * data, int datalen)
 {
-	int k ;
+    int k;
 
-	/*
-	**	Hann window function from :
-	**
-	**	http://en.wikipedia.org/wiki/Window_function
-	*/
+    /*
+     *	Hann window function from :
+     *
+     *	http://en.wikipedia.org/wiki/Window_function
+     */
 
-	for (k = 0 ; k < datalen ; k++)
-		data [k] = 0.5 * (1.0 - cos (2.0 * M_PI * k / (datalen - 1))) ;
+    for (k = 0; k < datalen ; k++) {
+	data[k] = 0.5 * (1.0 - cos(2.0 * M_PI * k / (datalen - 1)));
+    }
 
-	return ;
-} /* calc_hann_window */
-
-/*==============================================================================
-*/
+    return;
+}
 
 static double
-besseli0 (double x)
-{	int k ;
-	double result = 0.0 ;
+besseli0(double x)
+{
+    int k;
+    double result = 0.0;
 
-	for (k = 1 ; k < 25 ; k++)
-	{	double temp ;
+    for (k = 1; k < 25; k++) {
+    	double temp;
 
-		temp = pow (0.5 * x, k) / factorial (k) ;
-		result += temp * temp ;
-		} ;
+	temp = pow(0.5 * x, k) / factorial(k);
+	result += temp * temp;
+    }
 
-	return 1.0 + result ;
-} /* besseli0 */
+    return 1.0 + result;
+}
 
 static double
-factorial (int val)
-{	static double memory [64] = { 1.0 } ;
-	static int have_entry = 0 ;
-
-	int k ;
-
-	if (val < 0)
-	{	printf ("Oops : val < 0.\n") ;
-		exit (1) ;
-		} ;
-
-	if (val > ARRAY_LEN (memory))
-	{	printf ("Oops : val > ARRAY_LEN (memory).\n") ;
-		exit (1) ;
-		} ;
-
-	if (val < have_entry)
-		return memory [val] ;
-
-	for (k = have_entry + 1 ; k <= val ; k++)
-		memory [k] = k * memory [k - 1] ;
-
-	return memory [val] ;
-} /* factorial */
-
-/*==============================================================================
-*/
-
-static void init_test (void) __attribute__ ((constructor)) ;
-
-static void
-init_test (void)
+factorial(int val)
 {
-	/* puts (__func__) ;*/
+    static double memory[64] = { 1.0 };
+    static int have_entry = 0;
 
-	assert (factorial (0) == 1.0) ;
-	assert (factorial (2) == 2.0) ;
-	assert (factorial (0) == 1.0) ;
-	assert (factorial (5) == 120.0) ;
-	assert (factorial (8) == 40320.0) ;
+    int k;
 
-	assert (fabs (besseli0 (0.0) - 1.0) < 1e-8) ;
-	assert (fabs (besseli0 (0.5) - 1.06348337074132) < 1e-8) ;
-	assert (fabs (besseli0 (1.0) - 1.26606587775201) < 1e-14) ;
-	assert (fabs (besseli0 (2.0) - 2.27958530233607) < 1e-14) ;
-	assert (fabs (besseli0 (3.5) - 7.37820343222548) < 1e-14) ;
+    if (val < 0) {
+	printf("Oops : val < 0.\n");
+	exit(1);
+    }
 
-} /* init_test */
+    if (val > ARRAY_LEN(memory)) {
+	printf("Oops : val > ARRAY_LEN(memory).\n");
+	    exit(1);
+    }
 
+    if (val < have_entry)
+	    return memory[val];
+
+    for (k = have_entry + 1; k <= val ; k++)
+	    memory[k] = k * memory[k - 1];
+
+    return memory[val];
+}
