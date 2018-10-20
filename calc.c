e2949501a9e43de0753f52b7192f85e5d9fd529b
Martin Guy <martinwguy@gmail.com>
2018-10-20 19:57:13 +0200

    Don't wait and retry if SDL-post-event fails.
    It caused more problems than it saved, as all calc threads ended up
    waiting, causing embarassing pauses.

--- calc.c
+++ calc.c
@@ -115,12 +115,12 @@ calc_result(result_t *result)
 	event.user.data1 = result;
 	if (SDL_PushEvent(&event) != 0) {
 	    /* The Event queue is full. let it empty and try again. */
-	    usleep(100000);
-	    if (SDL_PushEvent(&event) != 0) {
+	    //usleep(100000);
+	    //if (SDL_PushEvent(&event) != 0) {
 		fprintf(stderr, "Couldn't post a result event\n");
 		return;
 	    }
-	}
+	//}
     }
 #endif
 }
