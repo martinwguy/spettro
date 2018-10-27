39e19596341660ef07281f4e1ed74f5ba8d56192
Martin Guy <martinwguy@gmail.com>
2018-10-27 17:52:59 +0200

    Add locks for audio_file round call from calc thread
    The locks were missing round the file reader in the calculation thread.

--- calc.c
+++ calc.c
@@ -27,6 +27,7 @@
 #include "cache.h"
 #include "calc.h"
 #include "gui.h"	/* For RESULT_EVENT */
+#include "lock.h"
 #include "speclen.h"
 #include "spectrum.h"
 #include "main.h"	/* for window_function */
@@ -134,9 +135,17 @@ get_result(calc_t *calc, spectrum *spec, double t)
 
 	/* Fetch the appropriate audio for our FFT source */
 	/* The data is centred on the requested time. */
+	if (!lock_audio_file()) {
+	    fprintf(stderr, "Cannot lock audio file\n");
+	    exit(1);
+	}
 	read_audio_file(calc->audio_file, (char *) spec->time_domain,
 			af_double, 1,
 			lrint(t * calc->sr) - fftsize/2, fftsize);
+	if (!unlock_audio_file()) {
+	    fprintf(stderr, "Cannot unlock audio file\n");
+	    exit(1);
+	}
 
 	calc_magnitude_spectrum(spec);
 
