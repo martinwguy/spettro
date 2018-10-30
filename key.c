0672028fc90958bc6d3c2c8805f591f88e001253
Martin Guy <martinwguy@gmail.com>
2018-10-30 01:20:45 +0100

    In SDL2, use a mixture of TEXTINPUT and KEYDOWN events
    TEXTINPUT gets shift-+ right (* on this keyboard) and AltGr too but
    doesn't handle arrow keys, Home, End or keypad with NumLock off, while
    KEYDOWN does these but doesn't handle AltGr and Shift for the keyboard
    layout.
    
    By using a combination of both, we get the best of both.

--- key.c
+++ key.c
@@ -13,7 +13,20 @@ bool Shift, Control;
 /*
  * Keypress events
  *
- * Other interesting ecore key names are:
+ * Ecore works OK with all keys.
+ * SDL1 (KEYDOWN event) doesn't apply Shift or AltGr to key presses,
+ *	so you can't get keys only available with Shift or AltGr
+ *	(e.g. If the keyboard's * is shift-+, you can't use it for brightness)
+ *	Apparently this is because that's more suitable for games.
+ * SDL2's KEYDOWN events are the same, but it also has a TEXTINPUT event
+ *	which does respond to Shift and AltGr, but ignores
+ *	Home, End, PgUp, PgDown and the keypad arrow keys.
+ *	We enable both types of events for SDL2 and jockey between them
+ *	hoping to get all keypresses but no duplicates!
+ *	The TEXTINPUT event ignores the keypad, the arrow keys and
+ *	Control-X combinations.
+ *
+ * Other interesting Ecore key names:
  *	"XF86AudioPlay"	Media button >
  *	"XF86AudioStop"	Media button []
  *	"XF86AudioPrev"	Media button <<
@@ -31,12 +44,31 @@ sdl_keydown(SDL_Event *eventp)
 #if ECORE_MAIN
     Evas_Event_Key_Down *ev = einfo;
     const Evas_Modifier *mods = evas_key_modifier_get(evas);
-    char *name = strdup(ev->key);	/* Copy because we'll lower-case it */
-#elif SDL_MAIN
-    char *name = strdup(SDL_GetKeyName(eventp->key.keysym.sym));
+#elif SDL_MAIN && SDL2
+    bool numlock;
 #endif
 
     enum key key = KEY_NONE;
+    char *name;
+
+#if ECORE_MAIN
+    name = strdup(ev->key);	/* Copy because we'll lower-case it */
+#elif SDL_MAIN
+# if SDL1
+    name = strdup(SDL_GetKeyName(eventp->key.keysym.sym));
+# elif SDL2
+    switch (eventp->type) {
+    case SDL_TEXTINPUT:
+    	name = strdup(eventp->text.text);
+	break;
+    case SDL_KEYDOWN:
+        name = strdup(SDL_GetKeyName(eventp->key.keysym.sym));
+    	break;
+    default:
+        abort();
+    }
+# endif
+#endif
 
 #if ECORE_MAIN
     Shift = evas_key_modifier_is_set(mods, "Shift");
@@ -44,13 +76,24 @@ sdl_keydown(SDL_Event *eventp)
 #elif SDL_MAIN
     Shift = !!(SDL_GetModState() & KMOD_SHIFT);
     Control = !!(SDL_GetModState() & KMOD_CTRL);
+# if SDL2
+    numlock = !!(SDL_GetModState() & KMOD_NUM);
+# endif
 #endif
+
+    map_to_lower_case(name);
 
     /* Handle single-character strings separately, not for speed but to avoid
      * tons of compiler warnings "array index 2 is past the end of the array"
      * due to glibc's stupid 17-line #define for strcmp().
      */
-    map_to_lower_case(name);
+#if SDL_MAIN && SDL2
+    /* Single-character presses are handled best by TEXTINPUT,
+     * but named characters and control characters only come by KEYDOWN
+     */
+    if (name[1] == '\0' && (eventp->type == SDL_KEYDOWN && !Control)) return;
+    if (eventp->type == SDL_TEXTINPUT || Control)
+#endif
     if (name[1] == '\0') switch (name[0]) {
 	case 'q': key = KEY_Q;			break;
 	case 'c': key = KEY_C;			break;
@@ -76,11 +119,19 @@ sdl_keydown(SDL_Event *eventp)
         case '-': key = KEY_MINUS;		break;
         case '*': key = KEY_STAR;		break;
         case '/': key = KEY_SLASH;		break;
+	/* SDL2 TEXTINPUT sends " " for space, but KEYDOWN send "space" too
+	 * so we use the latter rather than get two. */
+	case ' ': break;
 	default:
 	    goto moan;
     }
+    if (key != KEY_NONE) goto done;
+
+#if SDL_MAIN && SDL2
+    if (eventp->type == SDL_KEYDOWN) {
+#endif
     /* names common to several toolkits */
-    else if (!strcmp(name, "escape"))			key = KEY_ESC;
+         if (!strcmp(name, "escape"))			key = KEY_ESC;
     else if (!strcmp(name, "space"))			key = KEY_SPACE;
     else if (!strcmp(name, "left"))			key = KEY_LEFT;
     else if (!strcmp(name, "right"))			key = KEY_RIGHT;
@@ -112,7 +163,7 @@ sdl_keydown(SDL_Event *eventp)
     else if (!strcmp(name, "[1]"))			key = KEY_END;
     else if (!strcmp(name, "[2]"))			key = KEY_DOWN;
     else if (!strcmp(name, "[3]"))			key = KEY_PGDN;
-    else if (!strcmp(name, "[4]"))			key = KEY_LEFT;
+    else if (!strcmp(name, "[7]"))			key = KEY_HOME;
     else if (!strcmp(name, "[6]"))			key = KEY_RIGHT;
     else if (!strcmp(name, "[7]"))			key = KEY_HOME;
     else if (!strcmp(name, "[8]"))			key = KEY_UP;
@@ -124,18 +175,20 @@ sdl_keydown(SDL_Event *eventp)
 # elif SDL2
     else if (!strcmp(name, "pageup"))			key = KEY_PGUP;
     else if (!strcmp(name, "pagedown"))			key = KEY_PGDN;
-    else if (!strcmp(name, "keypad 1"))			key = KEY_END;
-    else if (!strcmp(name, "keypad 2"))			key = KEY_DOWN;
-    else if (!strcmp(name, "keypad 3"))			key = KEY_PGDN;
-    else if (!strcmp(name, "keypad 4"))			key = KEY_LEFT;
-    else if (!strcmp(name, "keypad 6"))			key = KEY_RIGHT;
-    else if (!strcmp(name, "keypad 7"))			key = KEY_HOME;
-    else if (!strcmp(name, "keypad 8"))			key = KEY_UP;
-    else if (!strcmp(name, "keypad 9"))			key = KEY_PGUP;
-    else if (!strcmp(name, "keypad +"))			key = KEY_PLUS;
-    else if (!strcmp(name, "keypad -"))			key = KEY_MINUS;
-    else if (!strcmp(name, "keypad *"))			key = KEY_STAR;
-    else if (!strcmp(name, "keypad /"))			key = KEY_SLASH;
+    /* If numlock is on, we get both a TEXTINPUT and a KEYDOWN */
+    else if (!strcmp(name, "keypad 1") && !numlock)	key = KEY_END;
+    else if (!strcmp(name, "keypad 2") && !numlock)	key = KEY_DOWN;
+    else if (!strcmp(name, "keypad 3") && !numlock)	key = KEY_PGDN;
+    else if (!strcmp(name, "keypad 4") && !numlock)	key = KEY_LEFT;
+    else if (!strcmp(name, "keypad 6") && !numlock)	key = KEY_RIGHT;
+    else if (!strcmp(name, "keypad 7") && !numlock)	key = KEY_HOME;
+    else if (!strcmp(name, "keypad 8") && !numlock)	key = KEY_UP;
+    else if (!strcmp(name, "keypad 9") && !numlock)	key = KEY_PGUP;
+    /* Ignore these as they also come in as a TEXTINPUT character "+" etc */
+    else if (!strcmp(name, "keypad +"))			key = KEY_NONE;
+    else if (!strcmp(name, "keypad -"))			key = KEY_NONE;
+    else if (!strcmp(name, "keypad *"))			key = KEY_NONE;
+    else if (!strcmp(name, "keypad /"))			key = KEY_NONE;
 # endif
 #endif
     /* Don't report shift/ctrl presses. "left shift" is SDL[12] */
@@ -152,7 +205,12 @@ sdl_keydown(SDL_Event *eventp)
 #endif
     else
 moan:	fprintf(stderr, "Key \"%s\" doesn't do anything.\n", name);
+#if SDL_MAIN && SDL2
+    }
+#endif
 
+done:
+    free(name);
     do_key(key);
 }
 
