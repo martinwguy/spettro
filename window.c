aebe308b9cc7845dd4175084ce67746eda133582
Martin Guy <martinwguy@gmail.com>
2019-02-02 23:31:37 +0100

    Add GPL2 copyright boilerplate

--- window.c
+++ window.c
@@ -1,5 +1,6 @@
 /*
 ** Copyright (C) 2007-2015 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Modified (M) 2018 by Martin Guy <martinwguy@gmail.com>
 **
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
